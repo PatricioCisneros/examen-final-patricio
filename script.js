647ab3e92b25cf360a25f13a788447a7088fa9c6
PatricioCisneros <al45760@un.edu.mx>
2025-12-02 10:41:09 -0600

    avance 6 en script pero no funciona

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ var masculino = true;
 
 var nombre = document.querySelectorAll("datos-personales") [0];
 
-var ocupacion = document.querySelectorAll("datos-personales") [0];
+var ocupacion = document.querySelectorAll("datos-personales") [1];
 
 var tarjeta = document.querySelector("perfil-card");
 
+tarjeta.addEventListener ("cliclk", function() {
+
+    if (masculino) {
+
+        nombre.textContent = "Oscar Patricio Guerra Cisneros";
+        ocupacion.textCotent = "Estudiante (Ella)";
+        alert("Cambio a femenino");
+
+    }
+
+}
